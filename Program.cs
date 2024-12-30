325afa02f927e4407aa37d17c4d0d0ffb9e6a52f
ahmedkamel6486 <ak1953292@gmail.com>
2024-12-30 18:59:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = 10;
+            int y = 20;
+            Console.WriteLine($"Equation : {x} + {y} = { x + y :C}");
+
+
+
+        }
+    }
+}
